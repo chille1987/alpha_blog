c49238079715c2f51dd6a17555e588ef4f251533
Denis Omerovic <denis.omerovic24@gmail.com>
2021-01-12 23:03:54 +0100

    Added autoprefixer, code refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages#index'
+  root 'articles#index'
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update] 
+  resources :articles
 end
