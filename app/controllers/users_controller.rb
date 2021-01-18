cd43b17a4ccf7d57c9084b950c7dfc703b1097f9
Denis Omerovic <denis.omerovic24@gmail.com>
2021-01-18 12:14:32 +0100

    Create ad Edit Users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}!"
+      redirect_to articles_path
+    else
+      render "new"
+    end    
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Account Updated!"
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
