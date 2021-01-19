16e81151e361c76137e309d04bae5cf31751257f
Denis Omerovic <denis.omerovic24@gmail.com>
2021-01-19 22:23:52 +0100

    Added pagination to articles and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,52 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end
+
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
+  end
+
+  def show
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}!"
-      redirect_to articles_path
+      redirect_to @user
     else
       render "new"
     end    
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Account Updated!"
-      redirect_to articles_path
+      redirect_to @user
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
   end
 
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
